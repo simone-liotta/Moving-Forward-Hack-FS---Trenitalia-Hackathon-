1ac77a92c579f1723c8125d09b23484107f5e01a
Simone Liotta <44676159+simone-liotta@users.noreply.github.com>
2019-05-08 00:29:48 +0200

    Add files via upload
    MATLAB code about a face detection and recognition, based on Local Binary Pattern algorithm and Support Vector Machine classifier. 
    The code is been realized in less than 36 hours, during the MOVING FORWARD HACK (Trenitalia/Ferrovie dello Stato Hackathon in LUISS Enlabs, Rome, 2017)

---
+++ LBP_Face_Recognition.m
@@ -0,0 +1,104 @@
+function bool = LBP_Face_Recognition(faccia_test)
+
+cd P_01
+
+percorso ='C:\Users\simo_\Desktop\Check In FS\P_01';
+fileFolder = fullfile(percorso);
+dirOutput = dir(fullfile(fileFolder,'*.jpg'));
+list = struct2cell((dirOutput));
+test_list=cell(1,(length(dirOutput)));
+
+n = length(dirOutput);
+
+% Create a detector object.
+faceDetector = vision.CascadeObjectDetector;
+
+if ~isempty(dirOutput)
+    %for g=1:1
+    
+    % test_list{1,g}=list{1,g};
+    %  sample_face = Database_Face_detection(test_list);
+    sample_face = imread(list{1,1});
+    
+    
+    bboxes = step(faceDetector, sample_face);
+    sample_face = imcrop(sample_face, bboxes);
+    
+    sample_face = rgb2gray(sample_face);
+    sample_face = imresize(sample_face,[128 128]);
+    % test_list{1,1}=[];
+    %end
+end
+
+numNeighbors = 8;
+NCelle =4; %16
+
+finestre = [NCelle NCelle];
+numCells = prod(size(sample_face))/ (prod(finestre));
+numBins = numNeighbors*(numNeighbors-1)+3;
+m = numCells * numBins;
+
+features_Vect = double(zeros(n,m));
+
+
+
+if ~isempty(dirOutput)
+    
+    for k=1:(length(dirOutput))
+        
+        test_list{1,k}=list{1,k};
+        I = imread(test_list{1,k});
+        
+        % Detect faces.
+        bboxes = step(faceDetector, I);
+        data_face = imcrop(I, bboxes);
+        % data_face = Database_Face_detection(test_list);
+        %I = imread(data_face);
+        I = rgb2gray(data_face);
+        I = imresize(I,[128 128]);
+        
+        Feature = extractLBPFeatures(I,'CellSize',finestre);%,'Normalization','None'
+        %Feature = extractLBPFeatures(I);
+        features_Vect(k,:)= Feature;
+    end
+end
+
+%vettore delle etichette di apprendimento [-1,+1] infatti linspace genera
+%un vettore lineare casuale della lunghezza di lq_list nell'intervallo
+%[-1,-1] e nell'intervallo [+1,+1] per hq_list. Ottengo cioè un vettore con
+%tanti 1 quante sono le immagini HQ e tanti -1 quante sono le immagini LQ
+trainI = features_Vect;
+%trainL = cell(1);
+
+%trainL =[1;-1];
+if  (rem(n,2)== 0)
+    trainL =[linspace(1,1,(n/2))';...
+        linspace(-1,-1,(n/2))'];
+else
+    trainL =[linspace(1,1,round(n/2))';...
+        linspace(-1,-1,(round(n/2)-1))'];
+end
+%trainL{1,1} = 1;
+%trainL{2,1} = -1;
+
+%calcolo infine il modello facedoil training secondo i parametri
+%della libreria svm
+modelL=svmtrain(trainL,trainI,'-s 0 -t 0 -b 1');
+
+Feature_test = double(extractLBPFeatures(faccia_test,'CellSize',finestre,'Normalization','None'));
+test_Label = linspace(1,1,1);
+%Feature = extractLBPFeatures(I);
+% test_features_Vect(k,:)= Feature_test;
+
+[predictLabelL,accuracyLI,decision_valueL]=svmpredict(test_Label,Feature_test,modelL,'-b 1');
+%applicazione per visualizzare l'esito della classificazione
+if predictLabelL==1 %predizioneLabel
+    bool=1;
+    %ui=imread('ui.bmp');
+    %imshow(ui);
+else
+    bool=0;
+    % er=imread('error.bmp');
+    %imshow(er);
+end
+end
---
+++ MF17_FULL.m
@@ -0,0 +1,96 @@
+%%          MOVING FORWARD HACK 2017 by TRENITALIA e CODEMOTION
+%                  LUISS ENLABS, Via Marsala 29/h, Roma
+%            TEAM composto da ADRIANO ZOLLINO e SIMONE LIOTTA
+%
+% Soluzione proposta nell'ambito della Challenge 1: CUSTOMER EXPERIENCE,
+%                  'Check in, check out a bordo treno'.
+% dal titolo:
+%        "Check in/out a bordo treno basata su sistema di rilevamento/
+%               riconoscimento facciale e geolocalizzazione"
+
+%% CAMERA 1 (SEAT)
+
+clc
+clear all
+LAT_0 = 123456;
+LONG_0 = 1435677;
+while (1==1)
+    data=arduinoGPS;
+    for h=1:(length(data)-1)
+        
+        LAT_CURR=double(data{1,h});
+        LONG_CURR=double(data{1,h+1});
+    end
+    
+    if (LAT_CURR == LAT_0 & LONG_CURR == LONG_0)
+        
+       rmdir P_01 s
+       movefile('Utente_due','P_01'); 
+        
+    end
+    
+[bbox,videoFrame] = Realtime_Face_Detection;
+
+if ~isempty(bbox)
+    
+    %FACE RECOGNITION
+    faccia_test = imcrop(videoFrame,bbox);
+    
+    
+    %imshow(faccia_test);
+    faccia_test = rgb2gray(faccia_test);
+    faccia_test = imresize(faccia_test,[128 128]);
+    %faccia_test=imread(faccia_test);
+    
+    bool = LBP_Face_Recognition(faccia_test);
+    
+    cd User_Interfaces
+    
+    if bool==1 %predizioneLabel
+        
+        ui=imread('on_board.bmp');
+        figure,imshow(ui)
+    else
+        
+        er=imread('error.bmp');
+        figure,imshow(er)
+    end
+%else
+%% %% CAMERA 2 (SEAT)
+%RL=1;
+  %  while (RL<10)
+    [bboxes,frame] = detect_camera_2;
+    
+    if (bool==1)  %sto seduto
+        fprintf("INTRUSO! \n");
+        %er=imread('error.bmp');
+       % figure,imshow(er)
+    else %se non sto seduto controlla se sono io
+        crop=imcrop(frame,bboxes);
+        faccia_test=imresize(crop,[128 128]);
+        bool = LBP_Face_Recognition(faccia_test);
+        if (bool==1) 
+            fprintf("OK! \n");
+        else
+            fprintf("INTRUSO! \n");
+        end
+    end
+   % RL=RL+1;
+ %   end
+else 
+        bool=1;
+        [bboxes,frame] = detect_camera_2;
+        if (bool==1)  %sto seduto
+        fprintf("INTRUSO! \n");
+    else %se non sto seduto controlla se sono io
+        crop=imcrop(frame,bboxes);
+        faccia_test=imresize(crop,[128 128]);
+        bool = LBP_Face_Recognition(faccia_test);
+        if (bool==1) 
+            fprintf("OK! \n");
+        else
+            fprintf("INTRUSO! \n");
+        end
+    end
+end
+end
